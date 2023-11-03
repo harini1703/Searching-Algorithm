f1de5dc1bcd67bf40ae886d0ef6fb4457b058f98
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2023-11-03 12:15:02 +0530

    Add files via upload

---
+++ Binary Search.java
@@ -0,0 +1,34 @@
+public class binarySearch   
+{
+    public static void main(String[] args)
+    {
+        int[] nums={1,8,16,58,100};
+        int n=8;
+        int ans=binarySearch(nums,n);
+        System.out.println(ans);
+    }
+    static int binarySearch(int[] arr,int n)
+    {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=low+high/2;//we can use this formula to nullify java int problem
+            //int mid=(low+high)/2;//int has fixed size,might be possible that
+            // start+end exceeds the range of number
+            if (n>arr[mid])
+            {
+                 low=mid+1;
+            }
+            else if(n<arr[mid])
+            {
+                high=mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
